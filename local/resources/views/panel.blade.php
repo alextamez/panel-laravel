d8294c177d241c042e2c8b3d0531c7d368a27845
Oscar Adrian Gonzalez Gamboa <gonzalez.gamboa1@gmail.com>
2016-10-20 19:17:47 -0500

    Se agrega Bootstrap entre otras librerias y archivos

---
+++ local/resources/views/panel.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.master')
+@section('title', 'Panel de Usuarios')
+
+<!--Barra de Navegacion-->
+@section('navbar')
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<img src="{{url('')}}/img/logo.png"/>
+		</div>
+		<div class="col-md-4"><div>
+		<div class="col-md-4">
+			Login
+		</div>
+	</div>
+</div>
+
+<!--Contenido-->
+@section('content')
+
+@stop
